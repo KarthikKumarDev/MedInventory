52c7d95e7b87348731ff2b82acf11c31b5ead6a6
karthikkumar1996 <jkkr.1996@gmail.com>
2019-08-08 20:30:04 +0530

    Add NavBar

--- med-inventory/src/App.js
+++ med-inventory/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from "./layout/NavBar"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to KK Sidha Clinic</h2>
-        </div>
-        <p className="App-intro">
-          Coming soon!
-        </p>
+      <div>
+        <NavBar />
+        <h3>Coming soon...</h3>
       </div>
     );
   }
