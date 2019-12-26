71112e2dac66cda337611292d0f7469e1690d1d6
kanimozhi <kanimozhi.v@techmango.net>
2019-12-26 12:42:06 +0530

    Purchase Module changes

---
+++ med-inventory/src/components/purchase-medicine/purchase-layout/previewPurchase.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import "./previewPurchase.css";
+
+
+class PreviewPurchase extends Component{
+  constructor(props){
+      super(props);
+      this.state ={
+          selectedData : this.props.medicinelistchoosen
+      }
+  }
+
+  render() {
+      return(
+          <>
+           {this.state.selectedData ?
+               <div className="preview-content-box">
+                    <TableContainer component={Paper}>
+                        <Table  size="small"  aria-label="simple table">
+                            <TableHead>
+                                <TableRow >
+                                    <TableCell class="table-cell-style" align="left">Name of Medicine</TableCell>
+                                    <TableCell class="table-cell-style" align="left">New Stock Count</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                            {this.props.medicinelistchoosen.map(item => (
+                                <TableRow key={item.name}>
+                                     <TableCell class="table-cell-style" align="left">{item.name}</TableCell>
+                                     <TableCell class="table-cell-style" align="left">{item.newStockCount}</TableCell>
+                                </TableRow>
+                            ))}
+                            </TableBody>
+                        </Table>
+                     </TableContainer>
+              </div>
+               : ""}
+          </>
+       )
+     }
+     componentWillMount(){
+        this.setState({ selectedData: true }, () => console.log("Preview"));
+     }
+ }
+
+export default PreviewPurchase;
---
+++ med-inventory/src/components/purchase-medicine/purchase-layout/purchaseLayout.js
@@ -0,0 +1,140 @@
+import React, { Component } from "react";
+import Card from "@material-ui/core/Card";
+import axios from "axios";
+import CardContent from "@material-ui/core/CardContent";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import "./purchaseLayout.css";
+import NumericEditor from "./numericEditor.jsx";
+import PreviewPurchase from "./previewPurchase";
+
+class purchaseLayout extends Component{
+  constructor(){
+    super();
+    this.gridApi={};
+    this.gridOptions = {
+      suppressCellSelection: true,
+      suppressPropertyNamesCheck: true,
+      accentedSort: true,
+      enableBrowserTooltips: true,
+      suppressPreventDefaultOnMouseWheel: true,
+      suppressDragLeaveHidesColumns: true,
+      headerHeight: 46
+    }
+
+    this.state = {
+      columnDefs: [
+        {
+          headerName: "NAME", field: "name" ,sortable: true, filter: true ,pinned:"left", width:180,
+          headerCheckboxSelection: true, checkboxSelection: true, lockPosition: true
+        }, {
+          headerName: "MANUFACTURER", field: "manufacturer",sortable: true,  width:150, filter: true
+        }, {
+          headerName: "PRICE", field: "mrp",sortable: true,  width:150, filter: true
+        },{
+          headerName : "CREATED BY", field :"CreatedBy",sortable: true, width:150, filter: true
+        },{
+          headerName : "CATEGORY" , field : "category" ,sortable : true , width:150, filter :true
+        },{
+          headerName : "CURRENT STOCK COUNT" , resizable :"true",field : "currentStockCount" , width:180, sortable : true , filter : true
+        },{
+          headerName : "NEW STOCK COUNT" , field : "newStockCount" , sortable : true , width:180, filter : true,
+          editable: true,cellEditor: "numericEditor",pinned: "right", lockPinned: true
+        }
+      ],
+      rowData: [],
+      rowSelection : "multiple",
+      singleEditClick : "true",
+      frameworkComponents: {
+        numericEditor: NumericEditor
+      },
+      selectedMedicineData : []
+    }
+  }
+  render(){
+      return(
+      <div>
+         {this.state.rowData  ? (
+            <Card className="card-style">
+                <CardContent>
+                 <div className="layout-style">
+                    <div
+                      className="ag-theme-balham grid-box"
+                      style={{
+                        height: '400px',
+                        width: '100%',
+                        padding: '50px'
+                      }}
+                    >
+                      <AgGridReact
+                              columnDefs={this.state.columnDefs}
+                              rowData={this.state.rowData}
+                              gridOptions={this.gridOptions}
+                              pagination="true"
+                              paginationPageSize="10"
+                              rowSelection={this.state.rowSelection}
+                              singleClickEdit={this.state.singleEditClick}
+                              frameworkComponents={this.state.frameworkComponents}
+                              onSelectionChanged={this.onSelectionChanged}
+                              onGridReady={this.onGridReady}
+                          >
+                      </AgGridReact>
+                    </div>
+                    <div className="preview-box">
+                      <PreviewPurchase medicinelistchoosen={this.state.selectedMedicineData} />
+                    </div>
+                  </div>
+                </CardContent>
+            </Card>
+         ):(
+           <CircularProgress />
+         )} 
+       </div>
+      )
+   }
+   onGridReady = params => {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+  };
+
+   componentWillMount() {
+    this.getMedicine();
+   } 
+
+   onSelectionChanged = params =>{
+    const selectedNodes = params.api.getSelectedNodes();
+    let medicineList = [];
+    selectedNodes.map( node =>{
+         medicineList.push({ ...node.data});
+    }) 
+    this.setState({selectedMedicineData : medicineList});
+   }
+   
+  handleAddMedicineToBill = medicine => {
+    let billMedicineList = this.state.billMedicineList;
+    if (!billMedicineList.find(item => item.MedicineId === medicine.MedicineId))
+      billMedicineList.push({ ...medicine, units: 1 });
+    this.setState({ billMedicineList });
+  };
+
+  getMedicine = () => {
+    axios
+      .get(
+        "https://sidls7kjne.execute-api.ap-south-1.amazonaws.com/staging/medicine"
+      )
+      .then(response => {
+        this.setState({ rowData: response.data.Items }, () =>
+          console.log(this.state.rowData)
+        );
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      });
+  };
+  
+ }
+
+export default purchaseLayout;
