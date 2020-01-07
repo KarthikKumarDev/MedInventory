3226ac652f9475d5d040ba49c2781e0cdce9c4f8
Karthik Kumar <jkkr.1996@gmail.com>
2020-01-07 20:35:08 +0530

    feat: Dedicated medicine search by name input

--- med-inventory/src/components/search-medicine/MedicineTable.js
+++ med-inventory/src/components/search-medicine/MedicineTable.js
@@ -1,58 +1,80 @@
 import React from "react";
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-balham.css';
-
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-balham.css";
 
 class MedicineTable extends React.Component {
   constructor(props) {
-  super(props);
-  this.gridOptions = {
-    suppressCellSelection: true,
-    suppressPropertyNamesCheck: true,
-    accentedSort: true,
-    enableBrowserTooltips: true,
-    suppressPreventDefaultOnMouseWheel: true,
-    suppressDragLeaveHidesColumns: true,
-    rowHeight: 40,
-    headerHeight: 46
+    super(props);
+    this.gridOptions = {
+      suppressCellSelection: true,
+      suppressPropertyNamesCheck: true,
+      accentedSort: true,
+      enableBrowserTooltips: true,
+      suppressPreventDefaultOnMouseWheel: true,
+      suppressDragLeaveHidesColumns: true,
+      rowHeight: 40,
+      headerHeight: 46
+    };
+    this.state = {
+      columnDefs: [
+        {
+          headerName: "NAME",
+          field: "name",
+          sortable: true,
+          filter: true,
+          pinned: true
+        },
+        {
+          headerName: "MANUFACTURER",
+          field: "manufacturer",
+          sortable: true,
+          filter: true
+        },
+        {
+          headerName: "PRICE",
+          field: "mrp",
+          sortable: true,
+          filter: true
+        },
+        {
+          headerName: "CREATED BY",
+          field: "CreatedBy",
+          sortable: true,
+          filter: true
+        },
+        {
+          headerName: "CATEGORY",
+          field: "category",
+          sortable: true,
+          filter: true
+        }
+      ],
+      rowData: props.data
+    };
   }
-  this.state = {
-    columnDefs: [
-      {
-        headerName: "NAME", field: "name" ,sortable: true, filter: true ,pinned :true
-      }, {
-        headerName: "MANUFACTURER", field: "manufacturer",sortable: true, filter: true
-      }, {
-        headerName: "PRICE", field: "mrp",sortable: true, filter: true
-      },{
-        headerName : "CREATED BY", field :"CreatedBy",sortable: true, filter: true
-      },{
-        headerName : "CATEGORY" , field : "category" ,sortable : true , filter :true
-      }
-    ],
-    rowData: props.data
+  render() {
+    return (
+      <div
+        className="ag-theme-balham"
+        style={{
+          height: "500px",
+          width: "1020px"
+        }}
+      >
+        <AgGridReact
+          columnDefs={this.state.columnDefs}
+          rowData={this.state.rowData}
+          gridOptions={this.gridOptions}
+          pagination="true"
+          paginationPageSize="10"
+        ></AgGridReact>
+      </div>
+    );
   }
-}
-  render(){
-      return (
-        <div
-          className="ag-theme-balham"
-          style={{
-            height: '500px',
-            width: '1020px'
-          }}
-        >
-          <AgGridReact
-            columnDefs={this.state.columnDefs}
-            rowData={this.state.rowData}
-            gridOptions={this.gridOptions}
-            pagination="true"
-            paginationPageSize="10"
-          >
-          </AgGridReact>
-        </div>
-      );
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ rowData: nextProps.data });
   }
 }
 
